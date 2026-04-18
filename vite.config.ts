a37e65b037b706056b2f62476d750b31c6eb842c
Pratik Kedar <141951998+PratikDevelops@users.noreply.github.com>
2026-04-18 18:10:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import path from "path" // <-- Add this
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // <-- This tells Vercel what @ means
+    },
+  },
   plugins: [
-    tanstackStart({
-      deployment: {
-        preset: 'vercel',
-      },
-    }),
+    tanstackStart(),
   ],
 })
